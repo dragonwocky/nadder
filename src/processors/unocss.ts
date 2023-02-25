4d683edbdae68f3af2a25ddfb1b627a9aba6348d
dragonwocky <thedragonring.bod@gmail.com>
2023-02-25 16:43:27 +1100

    feat: reimplement @unocss/transformer-compile-class

--- src/processors/unocss.ts
+++ src/processors/unocss.ts
@@ -1,6 +1,7 @@
 import {
   createGenerator,
   expandVariantGroup,
+  type StaticShortcut,
   type UnoGenerator,
   type UserConfig,
 } from "npm:@unocss/core@0.50.0";
@@ -12,32 +13,48 @@ import type { Element } from "../server/deps.ts";
 const cssResetUrl = "https://esm.sh/modern-normalize@1.1.0?css",
   cssReset = await fetch(cssResetUrl).then((res) => res.text());
 
-interface ProcessorConfig {
-  extendDefaults?: boolean;
-  /**
-   * ...
-   */
-  compileClasses?: boolean;
-  /**
-   * ...
-   */
-  inlineStyles?: boolean;
-}
+let uno: UnoGenerator;
+const shortcuts: StaticShortcut[] = [];
 
-let uno: UnoGenerator,
-  _compileClasses = false,
-  _inlineStyles = true;
-const setup = (config: UserConfig & ProcessorConfig) => {
-  _compileClasses = config.compileClasses ?? _compileClasses;
-  _inlineStyles = config.inlineStyles ?? _inlineStyles;
-  config.extendDefaults ??= true;
-  config.presets ??= [];
-  config.preflights ??= [];
-  if (config.extendDefaults) {
-    config.presets.unshift(presetUno(), presetIcons(), presetTypography());
-    config.preflights.unshift({ getCSS: () => cssReset });
+interface Config {
+  extendDefaults: boolean;
+  mode: "interpret" | "compile";
+  compilePrefix: string;
+  outputAs: "cssFile" | "styleTag";
+  styleTagId: `#${string}`;
+  cssFilePath: `${string}.css`;
+}
+const config: Config = {
+    extendDefaults: true,
+    mode: "interpret",
+    compilePrefix: "uno-",
+    outputAs: "cssFile",
+    styleTagId: "#uno",
+    cssFilePath: "/uno.css",
+  },
+  setup = (userConf: UserConfig & Partial<Config>) => {
+    Object.assign(config, userConf);
+    userConf.presets ??= [];
+    userConf.preflights ??= [];
+    userConf.shortcuts ??= [];
+    if (config.extendDefaults) {
+      userConf.presets.unshift(presetUno(), presetIcons(), presetTypography());
+      userConf.preflights.unshift({ getCSS: () => cssReset });
+    }
+    uno = createGenerator(userConf);
+    uno.config.shortcuts.push(...shortcuts);
+  };
+
+const hash = (str: string) => {
+  // https://github.com/unocss/unocss/blob/72b4306fe218b2469b54fa5ffe0e96444ab9f345/
+  // packages/transformer-compile-class/src/index.ts#L80
+  let hval = 0x811C9DC5;
+  for (let i = 0, l = str.length; i < l; i++) {
+    hval ^= str.charCodeAt(i);
+    hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) +
+      (hval << 24);
   }
-  uno = createGenerator(config);
+  return (`00000${(hval >>> 0).toString(36)}`).slice(-6);
 };
 
 const interpret = (className: string) => {
@@ -47,10 +64,24 @@ const interpret = (className: string) => {
     className = expandVariantGroup(className, ["-", ":", ""], expandDepth);
     return className.replace(/\s+/, " ");
   },
-  compile = (className: string) => {
+  compile = async (className: string) => {
     className = interpret(className);
-    // ...
-    return className;
+    // e.g. dark:(font-bold m(x-1 y-2) border-(red-200 1)) my-classname
+    // --> uno-qx2pmx my-classname
+    const utilities: string[] = [],
+      unknown: string[] = [],
+      sort = className.split(/\s+/).map(async (token) => {
+        if (await uno.parseToken(token)) utilities.push(token);
+        else unknown.push(token);
+      });
+    await Promise.all(sort);
+    if (utilities.length) {
+      const classes = utilities.join(" "),
+        alias = `${config.compilePrefix}${hash(classes)}`;
+      shortcuts.push([alias, classes]);
+      uno.config.shortcuts.push([alias, classes]);
+      return `${[alias, ...unknown].join(" ")}`;
+    } else return className;
   };
 
 export default (async (document) => {
@@ -58,13 +89,13 @@ export default (async (document) => {
   const classNames = [],
     classElems = document.querySelectorAll("[class]");
   for (const elem of [...classElems] as Element[]) {
-    elem.className = _compileClasses
-      ? compile(elem.className)
+    elem.className = config.mode === "compile"
+      ? await compile(elem.className)
       : interpret(elem.className);
     classNames.push(elem.className);
   }
   const { css } = await uno.generate(classNames.join(" "));
-  if (_inlineStyles) {
+  if (config.outputAs === "styleTag") {
     const styleElem = document.querySelector("style#uno");
     if (styleElem) styleElem.innerText += css;
     else document.head.innerHTML += `<style id="uno">${css}</style>`;
