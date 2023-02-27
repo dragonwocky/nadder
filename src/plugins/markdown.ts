1bdff89c3cf3069858ff3d9d9c3710da3105e1e5
dragonwocky <thedragonring.bod@gmail.com>
2023-02-27 12:25:28 +1100

    fix: apply remark process type to md var

--- src/plugins/markdown.ts
+++ src/plugins/markdown.ts
@@ -1,12 +1,12 @@
 import type { Filter, Renderer } from "../server.ts";
-import { type Plugin, unified } from "npm:unified@10.1.2";
+import { type Plugin, type Processor, unified } from "npm:unified@10.1.2";
 import remarkParse from "npm:remark-parse@10.0.1";
 import remarkGfm from "npm:remark-gfm@3.0.1";
 import remarkRehype from "npm:remark-rehype@10.1.0";
 import rehypeSanitize from "npm:rehype-sanitize@5.0.1";
 import rehypeStringify from "npm:rehype-stringify@9.0.3";
 
-let md = unified()
+let md: Processor = unified()
   .use(remarkParse)
   .use(remarkGfm)
   .use(remarkRehype)
